2899735c885ea38310d1f1d234f0cd566138e173
mnoskov <mnoskov@naumen.ru>
2023-03-23 18:33:31 +0500

    Добавил вывод входных параметров

--- src/main/java/org/example/TaskExample.java
+++ src/main/java/org/example/TaskExample.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.stream.Collectors;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
@@ -8,6 +10,11 @@ public class TaskExample implements JavaDelegate
     @Override
     public void execute(DelegateExecution execution) throws Exception
     {
-        System.out.println("Hello");
+        System.out.println("Task %s complete with args: %n%s".formatted(
+                getClass().getSimpleName(),
+                execution.getVariables().entrySet().stream()
+                        .map(entry -> entry.getKey() + ':' + entry.getValue())
+                        .collect(Collectors.joining(","))
+        ));
     }
 }
